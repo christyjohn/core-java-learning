32359272d28ab3ba5505c879b538ee808506be54
Christy John <christyjohn.crz@gmail.com>
2026-09-16 21:29:25 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Learning the Java Language
+
+Utilising various resources to learn the Java Language
+
+## Resources used
+
+- Core Java by Cay Horstmann Vol I and Vol II
+- Udemy courses
---
+++ src/main/java/com/christyjohn/p1/c01_java_env/welcome/Welcome.java
@@ -0,0 +1,11 @@
+package com.christyjohn.p1.c01_java_env.welcome;
+
+public class Welcome {
+    public static void main(String[] args) {
+        String greeting = "Welcome to Core Java!";
+        System.out.println(greeting);
+        for (int i = 0; i < greeting.length(); i++)
+            System.out.print("=");
+        System.out.println();
+    }
+}
